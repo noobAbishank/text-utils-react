bdd76f51b95f346cee01c40d4de4948b4df310e7
noobAbishank <89622903+noobAbishank@users.noreply.github.com>
2021-08-27 14:39:54 +0530

    Update Navbar.js

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import {
+// import {
     
-    Link
-  } from "react-router-dom";
+//     Link
+//   } from "react-router-dom";
 
 
 export const Navbar = (props) => {
@@ -11,18 +11,18 @@ export const Navbar = (props) => {
         <div>
             <nav className= {`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} border border-${props.mode==='light'?'dark':'light'}`}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><img src="./android-chrome-512x512.png" alt="" />{props.navBrand}</Link>
+                    <a className="navbar-brand" href="#"><img src="./android-chrome-512x512.png" alt="" />{props.navBrand}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">{props.home}</Link>
+                                <a className="nav-link active" aria-current="page" href="#">{props.home}</a>
                             </li>
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <Link className="nav-link" to="/about">{props.about}</Link>
-                            </li>
+                            </li> */}
 
                         </ul>
                         {/* <form className="d-flex">
@@ -30,9 +30,9 @@ export const Navbar = (props) => {
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form> */}
                         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                            <input className="form-check-input" type="checkbox" id="mode" onClick= {props.toggler} />
-                            <label className="form-check-label" htmlFor="mode">Enable DarkMode</label>
-                        </div>
+                             <input className="form-check-input" type="checkbox" id="mode" onClick= {props.toggler} />
+                             <label className="form-check-label" htmlFor="mode">Enable DarkMode</label>
+                         </div>
 
                     </div>
                 </div>
